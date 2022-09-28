1981eaaba251d27a559c7c354850456c51376134
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-28 16:22:25 -0400

    Time: 2072 ms (6.93%), Space: 28 MB (39.83%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,12 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
-        dp = [nums[0]] #keep track of the best sum up to each index
-        for i in range(1,len(nums)):
-            if dp[-1] < 0: #if the previous max subarray is negative
-                dp.append(nums[i]) # we start our subarray sum fresh with just nums[i]
-            else: #if the previous sum was positive, we want to add it
-                dp.append(nums[i] + dp[-1])
-            maxSum = max(maxSum,dp[-1])
-        return maxSum
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0: #if the previous sum is positive
+                nums[i] += nums[i-1] #add it to this element
+            ans = max(ans,nums[i]) #maxmimize answer
+        return ans 
             
