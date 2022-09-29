40c7fc289b69f820439eaab2cb2aac3cfa598368
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 14:54:22 -0400

    Time: 562 ms (50.38%), Space: 14.7 MB (8.83%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,23 @@
 class Solution(object):
+    def binarySearch(self, nums, target): 
+        #standard binary search method with slight modifications to deal with the format of [number, index]
+        left,right=0,len(nums)
+        while left<right:
+            middle = (left+right)//2
+            element = nums[middle][0] #make sure we are comparing the number to target
+            if element == target:
+                return nums[middle][1] #return the original index
+            elif element>target:
+                right = middle
+            else:
+                left = middle+1
+        return -1
+    
     def twoSum(self, nums, target):
-        for i, num in enumerate(nums): #i is the index, num is the corresponding number (nums[i])
-            for j in range(i+1,len(nums)): #search through the rest of the array
-                if num + nums[j] == target: #if we found the pair which sums up to target
-                    return [i,j] #return it
+        numsWithIndex = [[num,i] for i,num in enumerate(nums)] #this array will keep track of the original index of each number
+        numsWithIndex.sort() #sort by the number value        
+        for i, [num, j] in enumerate(numsWithIndex): #i is the index in numsWithIndex, num is the corresponding number, and j is the original index
+            binSrchResult = self.binarySearch(numsWithIndex[i+1:],target-num) #search for target-num in the rest of the array
+            if binSrchResult != -1:
+                return [j,binSrchResult] #return the indices
             
