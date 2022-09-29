4202279fae27393ac247a18bed84261fc360d165
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 15:02:03 -0400

    Time: 116 ms (54.89%), Space: 14.8 MB (8.83%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,23 +1,16 @@
 class Solution(object):
-    def binarySearch(self, nums, target): 
-        #standard binary search method with slight modifications to deal with the format of [number, index]
-        left,right=0,len(nums)
-        while left<right:
-            middle = (left+right)//2
-            element = nums[middle][0] #make sure we are comparing the number to target
-            if element == target:
-                return nums[middle][1] #return the original index
-            elif element>target:
-                right = middle
-            else:
-                left = middle+1
-        return -1
-    
     def twoSum(self, nums, target):
         numsWithIndex = [[num,i] for i,num in enumerate(nums)] #this array will keep track of the original index of each number
         numsWithIndex.sort() #sort by the number value        
-        for i, [num, j] in enumerate(numsWithIndex): #i is the index in numsWithIndex, num is the corresponding number, and j is the original index
-            binSrchResult = self.binarySearch(numsWithIndex[i+1:],target-num) #search for target-num in the rest of the array
-            if binSrchResult != -1:
-                return [j,binSrchResult] #return the indices
+        left, right = 0, len(nums)-1 #left and right pointers at either end of the array
+        while True:
+            leftNum = numsWithIndex[left][0] #left number
+            rightNum = numsWithIndex[right][0] #right number
+            total = leftNum + rightNum #their sum
+            if total == target: #if they sum to the target
+                return [numsWithIndex[left][1],numsWithIndex[right][1]] #return the indices
+            elif total > target: #if the sum is too big, we need to make the right number smaller
+                right -= 1
+            else: #if the sum is too small, we make the left number bigger
+                left += 1
             
