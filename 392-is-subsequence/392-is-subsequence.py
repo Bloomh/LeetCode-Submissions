5b139495038a3c12fd04bb775b683ce07d86b079
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 17:27:18 -0400

    Time: 38 ms (85.90%), Space: 13.9 MB (42.92%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,11 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i = 0 #this index is for t
-        j = 0 #this index is for s
-        n = len(s) #length of s
-        m = len(t) #length of t
-        while j<n and n-j<=m-i: #j<n checks that we are within s, while n-j<=m-i checks that there are more characters left to check than we have characters left to find
-            if s[j] == t[i]: #if the characters we are looking at are equal
-                j+=1 #we found the jth character, so we increase j by 1
-            i+=1 #look at the next character in t
-        return j>=n #see if we found all the characters in s
+        i = 0                   #this index is for t
+        j = 0                   #this index is for s
+        n = len(s)              #length of s
+        m = len(t)              #length of t
+        while j<n and i<m:      #check that we are within bounds for t and s
+            if s[j] == t[i]:    #if the characters we are looking at are equal
+                j+=1            #we found the jth character, so we increase j by 1
+            i+=1                #look at the next character in t
+        return j>=n             #see if we found all the characters in s
