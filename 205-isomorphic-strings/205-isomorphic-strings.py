f7487bb20f83f1aae20e12cd460844b9ef669a4f
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 19:53:16 -0400

    Time: 138 ms (6.71%), Space: 14.2 MB (45.53%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -2,7 +2,10 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_to_t = {} #letters in s mapped to letters in t
         t_to_s = {} #t mapped to s
-        for char_s,char_t in zip(s,t): #go through characters in s and t
+        # for char_s,char_t in zip(s,t): #go through characters in s and t
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]  
             if char_s not in s_to_t and char_t not in t_to_s: #if neither element is mapped
                 s_to_t[char_s] = char_t #map s to t
                 t_to_s[char_t] = char_s #map t to s
