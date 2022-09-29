fe5d38727eae0ce389e78d00c9085451875310a1
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 15:05:38 -0400

    Time: 93 ms (62.52%), Space: 14.3 MB (67.65%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,16 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        numsWithIndex = [[num,i] for i,num in enumerate(nums)] #this array will keep track of the original index of each number
-        numsWithIndex.sort() #sort by the number value        
-        left, right = 0, len(nums)-1 #left and right pointers at either end of the array
-        while True:
-            leftNum = numsWithIndex[left][0] #left number
-            rightNum = numsWithIndex[right][0] #right number
-            total = leftNum + rightNum #their sum
-            if total == target: #if they sum to the target
-                return [numsWithIndex[left][1],numsWithIndex[right][1]] #return the indices
-            elif total > target: #if the sum is too big, we need to make the right number smaller
-                right -= 1
-            else: #if the sum is too small, we make the left number bigger
-                left += 1
+        seen = {} #dictionary to keep track of what values we have seen (stored as keys) and their index (stored as values)
+        for i, num in enumerate(nums): #i is the index, num is the number (nums[i])
+            if target-num in seen:
+                return [seen[target-num],i]
+            else:
+                seen[num] = i
             
