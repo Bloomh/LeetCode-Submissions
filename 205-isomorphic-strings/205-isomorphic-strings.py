d08287fabfa9ebff8d54180c189fd5ce0d61b0cc
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-09-29 20:05:18 -0400

    Time: 49 ms (84.57%), Space: 14.3 MB (45.53%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -2,15 +2,14 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_to_t = {} #letters in s mapped to letters in t
         t_to_s = {} #t mapped to s
-        # for char_s,char_t in zip(s,t): #go through characters in s and t
-        for i in range(len(s)):
-            char_s = s[i]
-            char_t = t[i]  
+        for char_s,char_t in zip(s,t): #go through characters in s and t
             if char_s not in s_to_t and char_t not in t_to_s: #if neither element is mapped
-                s_to_t[char_s] = char_t #map s to t
-                t_to_s[char_t] = char_s #map t to s
-            elif char_s in s_to_t and char_t in t_to_s and s_to_t[char_s] == char_t and t_to_s[char_t] == char_s: #otherwise if the characters in s and t are properly mapped to each other
-                continue #then we can continue
-            else: #at least one of the elements is mapped to something different!
-                return False #so we return false
+                s_to_t[char_s] = char_t 
+                t_to_s[char_t] = char_s 
+            else:
+                try: 
+                    if not (s_to_t[char_s] == char_t and t_to_s[char_t] == char_s):
+                        return False 
+                except: 
+                    return False #so we return false
         return True
