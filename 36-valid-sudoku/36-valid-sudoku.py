8063dc3894dd7de81404136fd7e254b4141d4976
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-02 14:47:54 -0400

    Time: 89 ms (99.41%), Space: 13.8 MB (82.26%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)] #
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-        for i in range(len(board)):
-            for j,num in enumerate(board[i]):
-                if num == ".":
+        rows = [set() for _ in range(9)] #sets for each row
+        cols = [set() for _ in range(9)] #sets for each column
+        boxes = [set() for _ in range(9)] #sets for each box
+        for i in range(len(board)): #go through each row
+            for j,num in enumerate(board[i]): #go through each element
+                if num == ".": #ignore any blank spaces
                     continue
-                boxNumber = (i//3)*3+(j//3)
-                if num in rows[i] or num in cols[j] or num in boxes[boxNumber]:
+                boxNumber = (i//3)*3+(j//3) #this is the number box
+                if num in rows[i] or num in cols[j] or num in boxes[boxNumber]: #if this is a duplicate in the row, column, or box, then it is an invalid board
                     return False
-                rows[i].add(num)
-                cols[j].add(num)
-                boxes[boxNumber].add(num)
+                rows[i].add(num) #add to the row
+                cols[j].add(num) #add to the column
+                boxes[boxNumber].add(num) #add to the box
         return True
