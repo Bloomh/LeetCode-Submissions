9dfc1b1d327c0ccaa840d99d700ffa93d1b012c7
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-01 21:29:21 -0400

    Time: 53 ms (99.59%), Space: 14.5 MB (70.32%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        dp = [[int(i<k) for i in range(target)]] + [[0]*target for _ in range(n-1)]
-        for i in range(1,n):
-            for j in range(min(k*(i+1),target)):
-                dp[i][j] += sum(dp[i-1][max(0,j-k):j])
-        return dp[-1][-1]%(10**9+7)
+        if target>n*k:
+            return 0
+        dp = [[int(i<k) for i in range(target)]] + [[0]*target for _ in range(n-1)] #
+        for i in range(1,n): #up until n
+            for j in range(min(k*(i+1),target)): #go to the end of what can be reached with i dice rolls
+                dp[i][j] += sum(dp[i-1][max(0,j-k):j]) #we can reach the sum j from any number between j-k and j-1
+        return dp[-1][-1]%(10**9+7) #return the answer mod 10^9+7
