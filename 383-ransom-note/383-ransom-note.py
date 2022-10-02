20686992dd4ef201ec717dab968dca89d358bd49
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-02 19:20:49 -0400

    Time: 73 ms (74.51%), Space: 14.2 MB (52.96%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,8 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomCounter = Counter(ransomNote)
-        magCounter = Counter(magazine)
-        for letter in set(ransomNote):
-            if ransomCounter[letter]>magCounter[letter]:
-                return False
-        return True
+        return not Counter(ransomNote) - Counter(magazine)
