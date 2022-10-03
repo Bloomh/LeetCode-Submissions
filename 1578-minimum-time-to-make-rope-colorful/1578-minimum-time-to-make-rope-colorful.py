2fb38b4cee302de9bd8d64a716bba35d88e5fa3a
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-02 21:43:53 -0400

    Time: 2598 ms (29.88%), Space: 23.6 MB (93.96%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,11 +1,11 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        ans = 0 #answer
-        for i in range(1,len(colors)): #go through all the colors except the first
+        ans = 0
+        for i in range(1,len(colors)): #go through all the balloons except the first
             if colors[i-1] == colors[i]: #if the color we are looking at equals the one before it
-                if neededTime[i] >= neededTime[i-1]:
-                    ans += neededTime[i-1] #remove the balloon that takes less time
-                else:
-                    ans += neededTime[i] #remove the balloon that takes less time
-                    neededTime[i] = neededTime[i-1] #we already removed the i balloon but we may want to remove the i-1 balloon, so move it to the i spot to access later
+                if neededTime[i] >= neededTime[i-1]: #if the previous balloon is easier to remove
+                    ans += neededTime[i-1] #remove the previous balloon
+                else: #if it is quicker to remove this balloon
+                    ans += neededTime[i] #remove it
+                    neededTime[i] = neededTime[i-1] #we already removed the balloon at i but we may want to remove the balloon at i-1, so move it to index i to access later
         return ans
