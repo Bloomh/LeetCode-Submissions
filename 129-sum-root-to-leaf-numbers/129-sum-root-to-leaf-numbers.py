4b15f9daa5d53e4f5066a29be599c87b54c386ae
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-03 22:28:07 -0400

    Time: 60 ms (31.94%), Space: 13.8 MB (66.20%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -13,8 +13,9 @@ class Solution:
                 amt = amt*10+node.val #update the corresponding amount for this root-to-leaf path
                 if not node.left and not node.right: #if it is a leaf
                     self.ans += amt #then add the path to our answer
-                dfs(node.left,amt) #search to the left
-                dfs(node.right,amt) #search to the right
+                else: #if it isn't a leaf
+                    dfs(node.left,amt) #search to the left
+                    dfs(node.right,amt) #search to the right
         
         dfs(root,0) #perform dfs on the root
         
