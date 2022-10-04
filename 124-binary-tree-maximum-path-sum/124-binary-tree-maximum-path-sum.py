fe2c5918156d9f6415fc3746992eee5db7d5c148
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-03 23:04:34 -0400

    Time: 183 ms (32.47%), Space: 21.4 MB (30.97%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,23 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self,node): #dfs
-        if not node:
-            return 0
-        left = self.dfs(node.left)
-        if left < 0:
-            left = 0
-        right = self.dfs(node.right)
-        if right < 0:
-            right = 0
-        if node.val + right + left > self.ans:
-            self.ans = node.val + right + left
-        if right < left:
-            right = left
-        return node.val + right
     def maxPathSum(self, root: TreeNode) -> int:
-        self.ans = root.val #answer  
-        self.dfs(root) #dfs on root
+        self.ans = root.val #answer
+        
+        def dfs(node): #dfs
+            if not node:
+                return 0
+            left = dfs(node.left)
+            if left < 0:
+                left = 0
+            right = dfs(node.right)
+            if right < 0:
+                right = 0
+            if node.val + right + left > self.ans:
+                self.ans = node.val + right + left
+            if right < left:
+                right = left
+            return node.val + right
+            
+            
+        dfs(root) #dfs on root
         return self.ans
 
         
