28b1d8e96a0314dc90ae7681ebf6dab062489a7c
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-03 23:02:58 -0400

    Time: 204 ms (17.85%), Space: 21.5 MB (30.97%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,28 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.ans = float('-inf') #answer
-        
-        def dfs(node): #dfs
-            if not node:
+    def maxPathSum(self, root: TreeNode) -> int:
+        max_path = float("-inf") # placeholder to be updated
+        def get_max_gain(node):
+            nonlocal max_path # This tells that max_path is not a local variable
+            if node is None:
                 return 0
-            left = dfs(node.left)
-            if left < 0:
-                left = 0
-            right = dfs(node.right)
-            if right < 0:
-                right = 0
-            if node.val + right + left > self.ans:
-                self.ans = node.val + right + left
-            if right < left:
-                right = left
-            return node.val + right
-            
-            
-        dfs(root) #dfs on root
-        return self.ans
-        
+
+            gain_on_left = max(get_max_gain(node.left), 0) # Read the part important observations
+            gain_on_right = max(get_max_gain(node.right), 0)  # Read the part important observations
+
+            current_max_path = node.val + gain_on_left + gain_on_right # Read first three images of going down the recursion stack
+            max_path = max(max_path, current_max_path) # Read first three images of going down the recursion stack
+
+            return node.val + max(gain_on_left, gain_on_right) # Read the last image of going down the recursion stack		
+        get_max_gain(root) # Starts the recursion chain
+        return max_path		
+
         
             
         
