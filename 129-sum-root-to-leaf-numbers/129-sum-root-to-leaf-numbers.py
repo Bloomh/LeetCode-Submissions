9227f28437c1fbe45ccebed94f6c43c8e3eac676
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-03 22:25:26 -0400

    Time: 59 ms (34.89%), Space: 13.9 MB (66.20%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
+        self.ans = 0 #answer
         
-        def dfs(node,amt):
-            if node:
-                if not node.left and not node.right:
-                    self.ans += amt*10+node.val
-                amt = amt*10+node.val
-                dfs(node.left,amt)
-                dfs(node.right,amt)
+        def dfs(node,amt): #depth-first search helper method
+            if node: #we only care if there is a node
+                amt = amt*10+node.val #update the corresponding amount for this root-to-leaf path
+                if not node.left and not node.right: #if it is a leaf
+                    self.ans += amt #then add the path to our answer
+                dfs(node.left,amt) #search to the left
+                dfs(node.right,amt) #search to the right
         
-        dfs(root,0)
+        dfs(root,0) #perform dfs on the root
         
-        return self.ans
+        return self.ans #return answer
