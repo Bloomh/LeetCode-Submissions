f3438491aa2c51195725b69fdb3bd265694bdb62
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-04 17:05:32 -0400

    Time: 36 ms (84.11%), Space: 13.9 MB (62.19%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,12 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        left,right = 0,n #we will look at the range [left,...,right]
+        left,right = 1,n #we will look at the range [left,...,right]
         while left<right: #while the range is more than 1 element
             mid = (left+right)//2 #middle of the range
             if isBadVersion(mid): #if this is a bad version then we want this to be the last possible index in our range
                 right = mid #stop looking after mid
             else: #if it is not a bad version then we need to look at versions after this one (so we start looking at mid+1)
                 left = mid+1
-        return left
+        return left #return the version we are looking at
             
