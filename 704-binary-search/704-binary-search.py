3b9e2368544d5bdfb6b06b2d7e53016356e4b9f4
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-04 16:35:54 -0400

    Time: 503 ms (12.11%), Space: 14.7 MB (25.93%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def search(self, nums, target):
-        left,right = 0,len(nums) #[left,left+1,...,right-1] is the area we are actually considering
-        while left<right: #while our range exists
-            mid = (left+right)//2 #middle of our range
-            num = nums[mid] #number at the middle
-            if num == target: #we found the integer
-                return mid
-            elif num > target: #number is too big so we should look at numbers in the range [left,...,mid-1]
-                right = mid
-            else: #number is too small so we should look at numbers in the range [mid+1,...,right-1]
-                left = mid+1
-        return -1 #didn't find the number
+        if not nums: #target can't be in an empty list
+            return -1
+        mid = len(nums)//2 #middle of the list
+        num = nums[mid] #number at the middle
+        if num == target: #we found the target!
+            return mid
+        elif num > target: #number is too big --> search the left part of the list
+            return self.search(nums[:mid],target)
+        else: #number is too small --> search the right of the list
+            srch = self.search(nums[mid+1:],target)
+            if srch == -1: #if the element wasn't found
+                return -1 #keep that result
+            return mid+1+srch #since the list passed into the new method doesn't have the first mid+1 elements, we need to add that back to the index
