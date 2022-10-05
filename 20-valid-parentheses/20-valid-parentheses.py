a2f653cfe6f4114a161d8072af9d5aae32a90249
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-05 15:35:54 -0400

    Time: 39 ms (25.11%), Space: 13.6 MB (56.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,15 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         stack = [] #stack will keep track of closing brackets we need
-        for char in s: #iterate over all characters
+        for i,char in enumerate(s): #iterate over all characters
             #first we will check to see if char is an opening bracket and if it is, then we add the corresponding closing bracket to stack
+            if len(stack) > len(s) - i:
+                return False
             if char == '(': 
                 stack.append(')')
             elif char == '[':
                 stack.append(']')
             elif char == '{':
                 stack.append('}')
-            elif len(stack) == 0 or not char == stack.pop():
+            elif len(stack) == 0 or char != stack.pop():
                 #if stack is empty or the last bracket is not the one we expect, then the parentheses are invalid
                 return False
         return len(stack) == 0 #if there are any closing brackets we still expected, then the parentehses are invalid
