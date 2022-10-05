9004f9677162a17d3599c51575aa0b0f0e8131c2
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-04 22:52:22 -0400

    Time: 130 ms (9.24%), Space: 16.8 MB (69.24%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.py
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
-        if depth == 1:
-            return TreeNode(val,root)
+        if depth == 1: #extra case
+            return TreeNode(val,root) #val is the value of the new root, left points to original tree
             
-        def dfs(node,depth):
-            if node:
-                if depth == 2:
-                    node.left = TreeNode(val,node.left)
-                    node.right = TreeNode(val,None,node.right)
-                else:
-                    dfs(node.left,depth-1)
-                    dfs(node.right,depth-1)
+        queue = collections.deque([(root,depth)]) #queue for bfs starts with the root
+        while queue: #while there are nodes to look at
+            node,depth = queue.popleft() #get the next node
+            if node: #if the node exists
+                if depth == 2: #if we need to add the nodes to the next level
+                    node.left = TreeNode(val,node.left) #add left
+                    node.right = TreeNode(val,None,node.right) #add right
+                else: #otherwise go to the next level
+                    queue.append((node.left,depth-1)) #add the left child
+                    queue.append((node.right,depth-1)) #add the right child
                     
-        dfs(root,depth)
-        return root
+        return root #return the new tree
