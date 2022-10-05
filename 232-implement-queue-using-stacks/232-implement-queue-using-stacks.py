79661f610f79c71d1a897ff870a00e00f8420a26
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-05 18:33:10 -0400

    Time: 67 ms (7.61%), Space: 14 MB (23.83%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -1,26 +1,28 @@
 class MyQueue:
 
     def __init__(self):
-        self.fwd = []
-        self.bck = []
+        self.fwd = [] #this stack will represent the front of the queue
+        self.bck = [] #this stack will represent the back of the queue
 
     def push(self, x: int) -> None:
-        self.bck.append(x)
+        self.bck.append(x) #add to the back
 
     def pop(self) -> int:
-        if not self.fwd:
-            while self.bck:
-                self.fwd.append(self.bck.pop())
-        return self.fwd.pop()
+        self.frontToBack()
+        return self.fwd.pop() #return the top element in the front
 
     def peek(self) -> int:
-        if not self.fwd:
-            while self.bck:
-                self.fwd.append(self.bck.pop())
-        return self.fwd[-1]
+        self.frontToBack()
+        return self.fwd[-1] #return the element at the front of our queue
 
     def empty(self) -> bool:
-        return len(self.fwd)+len(self.bck)==0
+        return len(self.fwd)+len(self.bck)==0 #if both our stacks are empty, the queue is empty
+    
+    def frontToBack(self) -> None:
+        if not self.fwd: #if we don't have anything at the front
+            while self.bck:
+                #add all elements from the back to the front
+                self.fwd.append(self.bck.pop())
 
 
 # Your MyQueue object will be instantiated and called as such:
