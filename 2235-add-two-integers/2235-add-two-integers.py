8c10ab77b07aa1c61d80d692612c18b5f83b00b6
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-06 00:31:06 -0400

    Time: 62 ms (19.71%), Space: 13.9 MB (8.82%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.py
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -1,3 +1,10 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        return num1+num2
+        if not (num1 and num2): #base case – one of the integers is zero
+            return num1 or num2 #return the one that exists
+        elif num1 < 0 and num2 < 0: #both negative numbers
+            return -self.sum(-num1,-num2) #fix to be positive
+        if num2 > num1:
+            num1, num2 = num2, num1 #make sure num1 is the bigger integer
+        return 1 + self.sum(num1-1,num2) #recursive call
+        
