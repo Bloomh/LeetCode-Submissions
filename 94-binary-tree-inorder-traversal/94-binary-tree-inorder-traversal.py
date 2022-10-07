635db6f80d365158427bab697578be46c5703baf
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-07 19:17:25 -0400

    Time: 66 ms (12.15%), Space: 13.7 MB (96.43%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,13 +6,4 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        self.ans = [] # answer
-
-        def dfs(node): # depth-first-search helper method
-            if node: # we only care about nodes that exist
-                dfs(node.left) # add the preorder traversal of the left node
-                self.ans.append(node.val) # add this node to the traversal
-                dfs(node.right) # add the preorder traversal of the right node
-
-        dfs(root) # start at the top
-        return self.ans
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
