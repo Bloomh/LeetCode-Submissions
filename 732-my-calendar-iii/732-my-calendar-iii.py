0e727fa7ceca7df3f9e97deaa47eb1831bb5b66e
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-07 00:32:18 -0400

    Time: 602 ms (87.69%), Space: 14.5 MB (66.15%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.py
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -5,7 +5,8 @@ class MyCalendarThree:
         # events will be stored in the form [time, start/end, k] where start is 1 and end is -1
         # k keeps track of the max k booking in this event
         self.calendar = [] 
-        self.max_k_booking = 1 # max k booking
+        
+        self.max_k_booking = 1 # max k booking (answer)
         
     # modified binary search for inserting an event into our calendar by time
     def searchInsert(self, calendar: List[int], target: int, start: bool) -> int: 
@@ -22,23 +23,23 @@ class MyCalendarThree:
         return l
 
     def book(self, start, end):
-        start_ind = self.searchInsert(self.calendar, start, True)
-        if start_ind > 0:
-            self.calendar.insert(start_ind,[start,True,self.calendar[start_ind-1][2]+1])
-            self.max_k_booking = max(self.max_k_booking, self.calendar[start_ind-1][2]+1)
-        else:
-            self.calendar.insert(start_ind,[start,True,1])
+        start_ind = self.searchInsert(self.calendar, start, True) # where to insert the start of this event
+        k_at_start = 1 # k-booking event at the start
+        if start_ind > 0: # if it doesn't start at the beginning
+            k_at_start = self.calendar[start_ind-1][2]+1 # the k-booking here is equal to the k-booking before it + 1
+        self.calendar.insert(start_ind,[start,True,k_at_start]) # insert this event
+        self.max_k_booking = max(self.max_k_booking, k_at_start) # update answer if needed 
         
-        end_ind = self.searchInsert(self.calendar, end, False)
+        end_ind = self.searchInsert(self.calendar, end, False) # where we will insert the end event
         
-        for ind in range(start_ind+1,end_ind):
-            event = self.calendar[ind]
-            event[2] += 1
-            self.max_k_booking = max(self.max_k_booking, event[2])
+        for ind in range(start_ind+1,end_ind): # from after the start event to where we will insert the end
+            event = self.calendar[ind] # event
+            event[2] += 1 # add 1 to the k-booking here since we started an event before here
+            self.max_k_booking = max(self.max_k_booking, event[2]) # update the answer
             
-        self.calendar.insert(end_ind,[end,False,self.calendar[end_ind-1][2]-1])
+        self.calendar.insert(end_ind,[end,False,self.calendar[end_ind-1][2]-1]) # add the end, which has one lower k-booking than the event before it
         
-        return self.max_k_booking
+        return self.max_k_booking # return the answer
 
 
 # Your MyCalendarThree object will be instantiated and called as such:
