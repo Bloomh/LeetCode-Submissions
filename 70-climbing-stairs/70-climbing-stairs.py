43766ea36a4d4764d6097470f5db4b52d664553a
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-08 14:50:55 -0400

    Time: 51 ms (51.87%), Space: 13.9 MB (56.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    @cache # store previous results
     def climbStairs(self, n):
-        if n <= 2: # there is 1 way to climb up 1 step and 2 ways to climb up 2 steps
-            return n
-        return self.climbStairs(n-1)+self.climbStairs(n-2) # we can get to this step from either of the last two steps
+        distinct_ways = [1,2] # 1 way to go up 1 step, 2 ways to go up 2 steps
+        for i in range(2,n): # go up to n steps
+            distinct_ways.append(distinct_ways[-1] + distinct_ways[-2]) # can come from either of the last two steps
+        return distinct_ways[n-1] # how many ways to go up n stairs
