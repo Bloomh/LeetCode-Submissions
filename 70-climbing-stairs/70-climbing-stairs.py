9b10f81335b18b657f87b695a2ad984cb4de702a
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-08 14:36:15 -0400

    Time: 59 ms (26.33%), Space: 14 MB (11.75%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    @cache # store previous results
+    def climbStairs(self, n):
+        if n <= 2: # there is 1 way to climb up 1 step and 2 ways to climb up 2 steps
+            return n
+        return self.climbStairs(n-1)+self.climbStairs(n-2) # we can get to this step from either of the last two steps
