aeb40f418c7625d29f94a013be0a677075bfcaef
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-08 00:25:38 -0400

    Time: 58 ms (50.09%), Space: 13.9 MB (9.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,6 +1,7 @@
 class Solution:
-    @cache
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n-1)+self.fib(n-2)
+        fib_numbers = [0,1] # initial two fib numbers
+        for i in range(2,n+1): # start at 2nd number, go up to nth
+            fib_numbers.append(fib_numbers[-1] + fib_numbers[-2]) # next fibonnaci number is a sum of the last two
+        return fib_numbers[n] # return nth fib number
+        
