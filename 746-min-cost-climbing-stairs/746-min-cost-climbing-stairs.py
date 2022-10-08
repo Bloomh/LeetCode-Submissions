9c18c51d5e8dbe3c96280ce590fcc5e897a67059
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-08 17:12:36 -0400

    Time: 121 ms (37.11%), Space: 14 MB (42.00%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        min_costs = [cost[0], cost[1]] # start with the starting steps
+        # here the 0th index is the second to last step and the 1st index is the last step
+        min_costs = (cost[0], cost[1]) # start with the starting steps
         
         for i in range(2, len(cost)): # from index 2 until the end of the staircase
-            min_costs.append(cost[i] + min(min_costs[i-1], min_costs[i-2]))
+            min_costs = (min_costs[1], cost[i] + min(min_costs[0], min_costs[1])) # the last step becomes the second to last step, the new last step uses our recursive relationship
         
-        return  min(min_costs[-1], min_costs[-2]) # since we can reach the end from either of the last two steps, we return the minimum from either step
+        return  min(min_costs) # since we can reach the end from either of the last two steps, we return the minimum from either step
             
         
