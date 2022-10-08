0faf8cb5628ed08459021bc616e5411b81645d8d
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-08 15:24:01 -0400

    Time: 4819 ms (80.68%), Space: 14.3 MB (12.36%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,36 +1,28 @@
 class Solution:
     def twoSumClosest(self,nums,target):
         l, r = 0, len(nums)-1
-        ans = nums[0] + nums[1]
-        diff = abs(ans - target)
+        ans = float('inf')
         while l<r:
             total = nums[l] + nums[r]
             if total == target:
                 return target
             else:
+                if abs(total-target) < abs(ans - target):
+                    ans = total
                 if total > target:
                     r -= 1
-                    if total - target < diff:
-                        ans = total
-                        diff = total - target
                 else:
                     l += 1
-                    if target - total < diff:
-                        ans = total
-                        diff = target - total
         return ans
     
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        ans = nums[0] + nums[1] + nums[2]
-        diff = abs(ans - target)
+        ans = float('inf')
         nums.sort()
         for i in range(len(nums)-2):
             last = nums.pop()
             twoClose = self.twoSumClosest(nums,target-last)
-            newDiff = abs(last + twoClose - target)
-            if newDiff < diff:
+            if abs(last + twoClose - target) < abs(ans - target):
                 ans = last + twoClose
-                diff = newDiff
             if ans == target:
                 return target
         return ans
