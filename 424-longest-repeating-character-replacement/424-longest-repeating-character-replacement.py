8099705a7e24cd7b329222f15be44a1200802ff7
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-09 17:32:18 -0400

    Time: 91 ms (90.60%), Space: 13.8 MB (71.16%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,20 +1,19 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        left,right = 0,k
-        substring = Counter(s[:k])
-        maxCount = 0
-        ans = k
+        left,right = 0,k # left and right of our sliding window
+        substring = Counter(s[:k]) # we know we can have at least k elements
+        maxCount = 0 # maximum count of a single element we have had in a window
+        ans = k # answer is at least k
         
-        n = len(s)
+        n = len(s) 
         while right < n:
-            substring[s[right]] += 1
-            if substring[s[right]] > maxCount: # if this is the most common element
-                maxCount = substring[s[right]]
-            if ans - maxCount < k:
-                ans += 1
+            substring[s[right]] += 1 # add the next element
+            maxCount = max(maxCount, substring[s[right]]) # update the maxCount if necessary
+            if ans < maxCount + k: # if we have space for another element that we can replace
+                ans += 1 # increment the answer
             else:
-                substring[s[left]] -= 1
-                left += 1
-            right += 1
+                substring[s[left]] -= 1 # otherwise, remove the last element in our window
+                left += 1 # move the end forward
+            right += 1 # move forward
             
         return ans
