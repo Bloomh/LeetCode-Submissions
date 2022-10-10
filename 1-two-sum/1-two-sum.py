d99a3288fbc23d793afed5e7c333b636a5f96c3e
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-09 22:35:28 -0400

    Time: 100 ms (60.37%), Space: 14.4 MB (21.95%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,8 +2,9 @@ class Solution(object):
     def twoSum(self, nums, target):
         seen = {} #dictionary to keep track of what values we have seen (stored as keys) and their index (stored as values)
         for i, num in enumerate(nums): #i is the index, num is the number (nums[i])
-            if target-num in seen:
-                return [seen[target-num],i]
+            needed = target-num #this is the amount we need to sum to target
+            if needed in seen: #if we have seen this amount then we found the pair!
+                return [seen[needed],i] #return the indices
             else:
-                seen[num] = i
+                seen[num] = i #add this number and its index into the dictionary
             
