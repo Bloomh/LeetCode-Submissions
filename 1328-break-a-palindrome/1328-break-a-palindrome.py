92f1c8aaa2ff1ace225bfce5ec73a929b4d54451
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-09 23:15:55 -0400

    Time: 36 ms (85.57%), Space: 13.9 MB (13.13%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,10 +1,10 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        n = len(palindrome)
-        if n == 1:
+        n = len(palindrome) # store palindrome length
+        if n == 1: # no way to make 1 letter not a palindrome
             return ""
-        for i, char in enumerate(palindrome):
-            if i == (n-1)/2 or char == "a":
+        for i, char in enumerate(palindrome): # go through palindrome
+            if i == (n-1)/2 or char == "a": # if right at the center, no change makes it not a palindrome; if it is "a", we don't want to change it since it is lexicographically small
                 continue
-            return palindrome[:i] + "a" + palindrome[i+1:]
-        return palindrome[:-1] + "b"
+            return palindrome[:i] + "a" + palindrome[i+1:] # replace character with an "a"
+        return palindrome[:-1] + "b" # if all "a", then replace the last one with a "b"
