4365fa1765f980c7a8127f0eb87746e0e2f16e88
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-11 10:55:01 -0400

    Time: 1531 ms (19.72%), Space: 24.6 MB (48.78%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,11 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        smallest = secondsmallest = float('inf')
-        for num in nums:
-            if num <= smallest:
-                smallest = num
-            elif num <= secondsmallest:
-                secondsmallest = num
-            else:
+        biggest = secondbiggest = float('-inf') # nums[k] and nums[j]
+        for i in range(len(nums)-1,-1,-1): # go through nums backward
+            if nums[i] >= biggest: # if it is the biggest element we've seen
+                biggest = nums[i] # set it to be nums[k]
+            elif nums[i] >= secondbiggest: # if it is the next biggest element
+                secondbiggest = nums[i] # set it to be nums[j]
+            else: # if num > nums[j] we have found a triplet!
                 return True
-        return False
+        return False # no triplet found
