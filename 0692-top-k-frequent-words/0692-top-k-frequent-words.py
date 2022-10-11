90d67a7929460cf7005ab1b517b9bf4c9679c661
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-11 19:24:16 -0400

    Time: 141 ms (9.95%), Space: 13.9 MB (64.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,3 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        return [i[0] for i in sorted(Counter(words).most_common(), key = lambda x:(-x[1],x[0]))[:k]]
+        count = Counter(words) # count of every word
+        arr = [(key,count[key]) for key in count] # get the key and value of everything in the counter
+        heapq.heapify(arr) # turn it into a heap – will sort by the count
+        return [element[0] for element in heapq.nsmallest(k,arr, key = lambda x:(-x[1],x[0]))] # get the k smallest elements, sorted primarily by the count (but negative so that bigger counts come first) and secondarily by the string         
