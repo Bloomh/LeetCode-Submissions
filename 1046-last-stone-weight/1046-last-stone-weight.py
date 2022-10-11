56af666cef2b25659dc2ae22134a954580abd784
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-11 18:55:07 -0400

    Time: 27 ms (98.51%), Space: 13.8 MB (96.82%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,8 +1,12 @@
 class Solution:
+    from heapq import heappush, heappop # push and pop elements while keeping them sorted
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones.sort()
-        while len(stones) >= 2:
-            newStone = stones.pop() - stones.pop()
-            if newStone:
-                bisect.insort(stones,newStone)
-        return stones[0] if stones else 0
+        arr = [] # new array
+        for stone in stones:
+            heappush(arr,-stone) # add the negative value of the stone to the heap since it is small to big
+        while len(arr) >= 2:
+            newStone = heappop(arr) - heappop(arr) # collide the two smallest stones (which are really the biggest since we made them negative)
+            if newStone: # if not zero
+                heappush(arr,newStone) # add the new stone
+        return -arr[0] if arr else 0 # if there are stones, return its true value (negative)
+            
