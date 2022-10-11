2797e38953414d8f8d41f3a735e1f59a3c583ebd
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-11 00:58:53 -0400

    Time: 1328 ms (36.32%), Space: 24.6 MB (48.78%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         n = len(nums)
-        if n < 3:
+        if n < 3: # can't have triplets if fewer than 3 elements
             return False
-        maxOne, maxTwo = nums[-1], float('-inf')
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i] < maxTwo:
+        maxOne, maxTwo = nums[-1], float('-inf') # maxOne is the max element from the right, maxTwo is the max value of nums[j] (middle element)
+        for i in range(len(nums)-2,-1,-1): # go through nums in reverse
+            if nums[i] < maxTwo: # found a triple!
                 return True
-            elif nums[i] > maxOne:
-                maxOne = nums[i]
-            elif maxOne > nums[i] > maxTwo:
-                maxTwo = nums[i]
-        return False
+            elif nums[i] > maxOne: # if this is the biggest element
+                maxOne = nums[i] # new maxOne
+            elif maxOne > nums[i] > maxTwo: # if this could go in between the elements
+                maxTwo = nums[i] # it is the new second max
+        return False # no triple found :(
