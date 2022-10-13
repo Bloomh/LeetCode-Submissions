dc645e323c558c8e18c8a1f5ca22b97266387580
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-13 15:22:11 -0400

    Time: 61 ms (54.61%), Space: 15 MB (8.06%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = collections.deque([(root,0)]) #queue of nodes to look at
-        ans = [] #answer queue
-        while queue: #while we have nodes to look at
-            node,level = queue.popleft() #get the next node and its level
-            if node: #we only care if there is a node --> it may be None, which we want to avoid
-                if level >= len(ans): #if there arent enough levels in our array
-                    ans = [[]] + ans #add a new level at the start
-                ans[-level-1].append(node.val) #add this value to the level (level goes from the back of the array since it is in reverse)
-                queue.append((node.left,level+1)) #add left child to queue
-                queue.append((node.right,level+1)) #add right child to queue
-        return ans # reverse result
+        self.ans = [] # answer
+        def dfs(node,level): # node and level it is on
+            if node: # we only care if the node exists
+                if level >= len(self.ans): # if we need to add another level
+                    self.ans = [[]] + self.ans # add new row to the beginning
+                self.ans[-level-1].append(node.val) # add this to the corresponding level
+                dfs(node.left,level+1) # check left subtree
+                dfs(node.right,level+1) # check right subtree
+        dfs(root,0) # search tree
+        return self.ans # reverse result
