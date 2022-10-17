e09db1e915fcb5f598f0eb9bd3eb621a160cb09e
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-16 20:20:35 -0400

    Time: 72 ms (9.05%), Space: 13.9 MB (11.66%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,3 +1,8 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        return len(set(sentence)) == 26
+        uniqueChars = set()
+        for letter in sentence:
+            uniqueChars.add(letter)
+            if len(uniqueChars) == 26:
+                return True
+        return False
