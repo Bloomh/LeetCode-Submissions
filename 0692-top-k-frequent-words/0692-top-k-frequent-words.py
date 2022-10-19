031084f21b31a8d0dbca6a5feec27ba099f8b2b0
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-18 20:34:32 -0400

    Time: 138 ms (14.75%), Space: 13.9 MB (94.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,8 +1,4 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        return [word for (word,count) in sorted(Counter(words).most_common(), key = lambda x:(-x[1],x[0]))[:k]] 
-    # Counter(words).most_common() gets us the words sorted by how common they are in the form (word, count)
-    # then we sort primarily by -x[1] which means -count since we want it to sort from biggest to smallest not smallest to biggest
-    # secondary sorting key is x[0], the word itself
-    # then we take just the first k elements using [:k]
-    # then we go through and just return the actual word for each element
+        c = Counter(words)
+        return heapq.nsmallest(k,set(words),key = lambda x:(-c[x],x))
