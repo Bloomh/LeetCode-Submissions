f9e7a6cde9b606103686a89c8d61ffb090b82403
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-20 14:44:31 -0400

    Time: 119 ms (18.54%), Space: 14 MB (6.54%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,43 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        ans = []
-        while num >= 900:
-            num -= 1000
-            ans += ['M']
-        if num < 0:
-            num += 100
-            ans.insert(-1,'C')
-        while num >= 400:
-            num -= 500
-            ans += ['D']
-        if num < 0:
-            num += 100
-            ans.insert(-1,'C')
-        while num >= 90:
-            num -= 100
-            ans += ['C']
-        if num < 0:
-            num += 10
-            ans.insert(-1,'X')
-        while num >= 40:
-            num -= 50
-            ans += ['L']
-        if num < 0:
-            num += 10
-            ans.insert(-1,'X')
-        while num >= 9:
-            num -= 10
-            ans += ['X']
-        if num < 0:
-            num += 1
-            ans.insert(-1,'I')
-        while num >= 4:
-            num -= 5
-            ans += ['V']
-        if num < 0:
-            num += 1
-            ans.insert(-1,'I')
-        ans += ['I'] * num
-        return ''.join(ans)
-        
-        
+        ans = ""
+        lettersToValues = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+        for letter, value in lettersToValues.items():
+            while num >= value:
+                num -= value
+                ans += letter
+        return ans
+                
