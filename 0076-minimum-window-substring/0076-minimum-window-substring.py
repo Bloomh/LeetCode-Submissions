04448b43b9ae0f16fc63f77d60cc0c0648f0f94d
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-21 22:48:06 -0500

    Time: 2189 ms (5.00%), Space: 14.7 MB (36.77%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,14 +1,14 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        tCounter = Counter(t)
-        window = Counter()
-        ans = ""
-        last = 0
+        tCounter = Counter(t) # counter for t to check with
+        window = Counter() # sliding window
+        ans = "" # answer
+        last = 0 # last index in our window
         for i,char in enumerate(s):
-            window[char] = window.get(char,0)+1
-            while window >= tCounter:
+            window[char] = window.get(char,0)+1 # add this character to our window
+            while window >= tCounter: # 
                 if ans == "" or i - last < len(ans):
                     ans = s[last:i+1]
-                window -= Counter(s[last])
+                window[s[last]] -= 1 # remove the last element from our counter
                 last += 1
         return ans
