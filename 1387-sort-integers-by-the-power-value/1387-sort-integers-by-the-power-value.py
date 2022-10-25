41c2ad4c329c753172012a77900e798b682d72dc
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-24 22:32:56 -0500

    Time: 747 ms (40.20%), Space: 15.7 MB (41.73%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        @lru_cache
+        def power(x):
+            if x == 1:
+                return 1
+            elif x % 2:
+                return 1 + power(3*x+1)
+            else:
+                return 1 + power(x//2)
+        return heapq.nsmallest(k, range(lo,hi+1), key = power)[-1]
