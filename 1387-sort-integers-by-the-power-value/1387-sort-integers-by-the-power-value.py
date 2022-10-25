e893d1d4071583d8e02d960ef1f382f1accbfba9
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-24 22:45:07 -0500

    Time: 297 ms (80.39%), Space: 21.7 MB (34.44%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -2,9 +2,5 @@ class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         @lru_cache(None)
         def power(x):
-            if x == 1:
-                return 1
-            elif x % 2:
-                return 1 + power(3*x+1)
-            return 1 + power(x//2)
+            return 1 if x ==1 else 1 + power(3*x+1) if x % 2 else 1 + power(x//2)
         return heapq.nsmallest(k, range(lo,hi+1), key = power)[-1]
