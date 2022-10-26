30197e6f161796f5ce1e1b4be814451854848c5d
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-26 17:54:35 -0500

    Time: 667 ms (28.00%), Space: 25.2 MB (97.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,17 +1,11 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start, end = start + 1, end - 1
         n = len(nums)
         k %= n
-        if k != 0: # we only want to rotate the array if we need to
-            if k < n//2: # if the end is less than half the size of the array
-                end = nums[-k:] # store the end of the array (which will become the front)
-                for i in range(n-k-1, -1, -1): # go backwards through the first n-k elements of the array
-                    nums[i+k] = nums[i] # shift everything forward k spots
-                nums[:k] = end # put the old end to the front
-            else: # the end is bigger than the beginning so store the beginning
-                beg = nums[:-k] # store the beginning!
-                for i in range(n-k, n): # go forwards through the last k elements
-                    nums[(i+k)%n] = nums[i] # shift them forwards k spots (to the front of the list)
-                nums[k:] = beg # set the end to the beginning
-                    
-                
+        reverse(0,n-1) # reverse full list
+        reverse(0,k-1) # reverse first k elements (previously the last k elements)
+        reverse(k,n-1) # reverse the rest of the list
