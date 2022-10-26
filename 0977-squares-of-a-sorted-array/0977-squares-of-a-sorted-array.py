f3a831e800551e2e13fcff34f86b5cc2b05ed646
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-25 23:28:48 -0500

    Time: 560 ms (33.33%), Space: 16 MB (87.17%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,29 +1,20 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        negatives = []
-        positives = []
-        for i,num in enumerate(nums):
-            if num < 0: # negative number
-                negatives = [num] + negatives # add to beginning of negatives
-            else:
-                positives = nums[i:]
-                break
+        zeroIndex = bisect.bisect(nums, 0) # use bisect's binary search to look for 0 (could also write a binary search method to search for 0)
+        neg, pos = zeroIndex-1, zeroIndex # negative pointer starts pointing before the 0, positive pointer starts pointing at it
+        n = len(nums)
         result = []
-        pos, neg = 0, 0
-        n, m = len(positives), len(negatives)
-        while pos < n and neg < m:
-            if positives[pos] <= -negatives[neg]: # if next positive number has a smaller or equal absolute value
-                result += [positives[pos]**2]
-                pos += 1
+        while 0 <= neg and pos < n:
+            if nums[pos] <= -nums[neg]: # if the positive pointer points to a smaller or equal abs val
+                result += [nums[pos]**2] # add smaller square
+                pos += 1 # move forward
             else:
-                result += [negatives[neg]**2]
-                neg += 1
-        while pos < n: # if there are positive numbers remaining
-            result += [positives[pos]**2] # add their squares
+                result += [nums[neg]**2] # add smaller square
+                neg -= 1 # move forward
+        while 0 <= neg: # add all remaining negative numbers
+            result += [nums[neg]**2]
+            neg -= 1
+        while pos < n: # add all remaining positive numbers
+            result += [nums[pos]**2]
             pos += 1
-        while neg < m: # if there are any negative numbers remaining
-            result += [negatives[neg]**2] # add their squares
-            neg += 1
-        return result # return the result
-                
-        
+        return result
