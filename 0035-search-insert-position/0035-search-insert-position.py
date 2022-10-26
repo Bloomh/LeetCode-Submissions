efb62537e9fc3a8a82e57c359ae9d5d5fbcdca12
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-25 22:10:53 -0500

    Time: 128 ms (5.39%), Space: 14.7 MB (40.09%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left,right = 0,len(nums) #[left,left+1,...,right-1] is the area we are actually considering
+        while left<right: #while our range exists
+            mid = (left+right)//2 #middle of our range
+            num = nums[mid] #number at the middle
+            if num == target: #we found the integer
+                return mid
+            elif num > target: #number is too big so we should look at numbers in the range [left,...,mid-1]
+                right = mid
+            else: #number is too small so we should look at numbers in the range [mid+1,...,right-1]
+                left = mid+1
+        return left #didn't find the number -- it should be inserted here
