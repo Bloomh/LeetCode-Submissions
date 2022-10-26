084f0383f4e031dd1cbefa587c041f741e9e7089
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-25 23:07:16 -0500

    Time: 1352 ms (5.01%), Space: 16.5 MB (5.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,3 +1,29 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return [num**2 for num in sorted(nums, key = abs)]
+        negatives = []
+        positives = []
+        for i,num in enumerate(nums):
+            if num < 0: # negative number
+                negatives = [num] + negatives # add to beginning of negatives
+            else:
+                positives = nums[i:]
+                break
+        result = []
+        pos, neg = 0, 0
+        n, m = len(positives), len(negatives)
+        while pos < n and neg < m:
+            if positives[pos] <= -negatives[neg]: # if next positive number has a smaller or equal absolute value
+                result += [positives[pos]**2]
+                pos += 1
+            else:
+                result += [negatives[neg]**2]
+                neg += 1
+        while pos < n: # if there are positive numbers remaining
+            result += [positives[pos]**2] # add their squares
+            pos += 1
+        while neg < m: # if there are any negative numbers remaining
+            result += [negatives[neg]**2] # add their squares
+            neg += 1
+        return result # return the result
+                
+        
