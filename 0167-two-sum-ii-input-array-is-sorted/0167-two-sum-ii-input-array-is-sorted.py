928e9720e310fa0ece5007390fbdffa8a3290573
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-26 20:50:42 -0500

    Time: 293 ms (43.50%), Space: 14.9 MB (40.82%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1 # left and right pointers
+        while left < right:
+            tot = numbers[left] + numbers[right] # sum of the elements at the two pointers
+            if tot == target: # if we found the total
+                return [left+1, right+1] # return the indices
+            elif tot > target: # if the total is too big
+                right -= 1 # make the right pointer point to a smaller element
+            else: # total is too small
+                left += 1 # make the left pointer point to a bigger element
