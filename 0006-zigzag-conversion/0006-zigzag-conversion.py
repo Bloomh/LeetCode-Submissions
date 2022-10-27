c336685f0d884f1e875d349b56ce2aa68f2cb82c
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-27 15:28:09 -0500

    Time: 46 ms (99.48%), Space: 14 MB (75.79%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -3,14 +3,14 @@ class Solution:
         if numRows == 1:
             return s
         n = len(s)
-        ans = s[::numRows*2-2]
-        for row in range(1,numRows-1):
-            i = row
-            increment = numRows*2 - 2 - row*2
-            while i < n:
-                ans += s[i]
-                i += increment
-                increment = numRows*2 - 2 - increment
-        return ans + s[numRows-1::numRows*2-2]
+        ans = s[::numRows*2-2] # start with the top row
+        for row in range(1,numRows-1): # get the middle rows
+            i = row # start at this row's first character
+            increment = numRows*2 - 2 - row*2 # this is how far away the next character in this row is
+            while i < n: # while in bounds
+                ans += s[i] # add this element
+                i += increment # jump to the next element in this row
+                increment = numRows*2 - 2 - increment # increment changes each time
+        return ans + s[numRows-1::numRows*2-2] # add the bottom row
             
             
