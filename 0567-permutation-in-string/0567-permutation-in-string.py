59eb27a3298668be571ba8df9c9205a719b3300f
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-28 13:31:44 -0500

    Time: 686 ms (25.89%), Space: 13.9 MB (68.06%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        c1 = Counter(s1)
-        c2 = Counter()
-        ln1 = len(s1)
-        for i, char in enumerate(s2):
-            c2 += Counter(char)
-            if i >= ln1:
-                c2 -= Counter(s2[i - ln1])
-            if c1 == c2:
+        c1 = Counter(s1) # keeps track of the number of times each character in s1 appears
+        c2 = Counter() # sliding window in s2
+        ln1 = len(s1) # length of s1 stored
+        for i, char in enumerate(s2): # for every character in s2
+            c2 += Counter(char) # add this character to our counter
+            if i >= ln1: # if we have looked at as many elements in s2 as there are in s1
+                c2 -= Counter(s2[i - ln1]) # remove the oldest one from our counter (we want the number of elements being considered from s2 to be equal to the number of elements in s1)
+            if c1 == c2: # if the counters are equal we found a permutation
                 return True
         return False
             
