5533c1523b77a958243ff0871950ee7f00825b4f
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-29 17:54:44 -0500

    Time: 49 ms (76.83%), Space: 13.9 MB (71.16%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,12 +5,17 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        nodes = [] # keep track of the nodes
-        while head: # while looking at a node
-            nodes.append(head) # add this node to our list
-            head = head.next # move to the next node
-        if n == len(nodes): # if the nth to last node is also the first node
-            return nodes[0].next # return the second node (we removed the first)
-        nodes[-n-1].next = nodes[-n-1].next.next # have the node before the nth from the end point to the node after it! –– this effectively removes the nth node from the end, nodes[-n]
-        return nodes[0] # return the first node
+        length = 0 # length
+        runner = head # runner to find length
+        while runner: # while the runner exists
+            length += 1 # add to the length
+            runner = runner.next # go to the next node
+        if length == n: # if we need to remove the first node
+            return head.next # remove it by returning the next node
+        runner = head # reset our runner
+        for i in range(length - n - 1): # until there are n nodes left in the linkedlist
+            runner = runner.next # move forward in the linkedlist
+        runner.next = runner.next.next # skip the next node (nth from the end)
+        return head # return the head
+        
         
