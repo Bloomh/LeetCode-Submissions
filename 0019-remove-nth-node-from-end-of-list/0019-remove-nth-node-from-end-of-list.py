f8607e3a668c95a8d6dbb2d70fb4d754e50a9f29
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-29 17:49:44 -0500

    Time: 23 ms (99.88%), Space: 13.9 MB (71.16%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -9,8 +9,9 @@ class Solution:
         while head: # while looking at a node
             nodes.append(head) # add this node to our list
             head = head.next # move to the next node
-        nodes.append(None) # add None to the end of our list to deal with the n == 1 case more easily
-        if n == len(nodes) - 1: # if the nth to last node is also the first node
+        if n == len(nodes): # if the nth to last node is also the first node
+            if n == 1: # if only 1 node
+                return None # there is no list remaining after we remove this node
             return nodes[1] # return the second node (we removed the first)
-        nodes[-n-2].next = nodes[-n] # have the node before the nth from the end point to the node after it! –– this effectively removes the nth node from the end, nodes[-n-1
+        nodes[-n-1].next = nodes[-n-1].next.next # have the node before the nth from the end point to the node after it! –– this effectively removes the nth node from the end, nodes[-n]
         return nodes[0] # return the first node
