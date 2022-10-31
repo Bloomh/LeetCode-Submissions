7a8e16af53a60f7e01e0f43b7f97cfa1c155f7e6
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-10-31 13:53:29 -0500

    Time: 223 ms (15.56%), Space: 13.9 MB (78.65%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,9 +1,9 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        nextrow = [num for i,num in enumerate(matrix[0][:-1])]
-        for row in matrix[1:]:
-            for i,num in enumerate(row[1:]):
-                if num != nextrow[i]:
-                    return False
-            nextrow = [row[0]] + nextrow[:-1]
+        nextrow = matrix[0][:-1] # we need all the elements in the next row (after the first element) to equal all the elements in this row except for the last element
+        for row in matrix[1:]: # for all rows except the first one
+            for i,num in enumerate(row[1:]): # for all elements in that row except the first one
+                if num != nextrow[i]: # if it isn't equal to the corresponding element in our "nextrow"
+                    return False # diagonal doesn't line up
+            nextrow = [row[0]] + nextrow[:-1] # the last element in "nextrow" gets removed and the first element of this row gets added to the beginning
         return True
