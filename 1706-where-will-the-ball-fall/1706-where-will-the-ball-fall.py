3b9322ff47ad7efbcd5dfc059e7a0fc5e8dbd4c8
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-01 16:00:27 -0500

    Time: 528 ms (30.32%), Space: 14.4 MB (31.16%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -5,10 +5,11 @@ class Solution:
         
         def path(i,j):
             direction = grid[i][j]
-            if not (0 <= j + direction < n and direction == grid[i][j+direction]):
+            j += direction
+            if not (0 <= j < n and direction == grid[i][j]):
                 return -1
             if i == m-1:
-                return j + direction
-            return path(i+1, j+direction)
+                return j
+            return path(i+1, j)
         
         return [path(0,i) for i in range(n)]
