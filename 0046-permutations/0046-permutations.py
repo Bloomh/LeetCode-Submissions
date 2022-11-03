d49f046c5fab5cd290bf75c112e5c734c421bc4d
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-03 17:52:42 -0500

    Time: 99 ms (7.54%), Space: 13.9 MB (84.86%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # store length of nums
         n = len(nums)
+        # if only one element
         if n == 1:
+            # return the one element
             return [nums]
+        # get the permutations of all the elements besides the first one
         perms = self.permute(nums[1:])
+        # initialize empty answer array
         ans = []
-        firstnum = nums[0]
         for perm in perms:
             for i in range(n):
                 copy = [num for num in perm]
-                copy.insert(i, firstnum)
+                copy.insert(i, nums[0])
                 ans.append(copy)
         return ans
