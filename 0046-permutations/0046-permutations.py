dbdc704b348a11edb00d7143a6bc2260e3238a40
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-03 17:51:18 -0500

    Time: 77 ms (55.82%), Space: 14.1 MB (60.12%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,2 +1,14 @@
 class Solution:
-    permute = itertools.permutations
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        if n == 1:
+            return [nums]
+        perms = self.permute(nums[1:])
+        ans = []
+        firstnum = nums[0]
+        for perm in perms:
+            for i in range(n):
+                copy = [num for num in perm]
+                copy.insert(i, firstnum)
+                ans.append(copy)
+        return ans
