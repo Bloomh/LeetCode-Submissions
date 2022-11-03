5e03f9f2b0d0a0d4ba3272b51ec25b5b357ad4ba
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-03 17:55:22 -0500

    Time: 71 ms (66.96%), Space: 14.2 MB (23.77%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -10,9 +10,13 @@ class Solution:
         perms = self.permute(nums[1:])
         # initialize empty answer array
         ans = []
+        # store the first number in the array (we will use this number a lot so storing it makes sense but is optional)
+        firstnum = nums[0]
+        # for every permutation in our permutations above
         for perm in perms:
+            # we can add firstnum at any index in the permutation, which is of length n-1
+            # therefore, it can be added at index 0, 1, ... n-1 (at the end of the list)
             for i in range(n):
-                copy = [num for num in perm]
-                copy.insert(i, nums[0])
-                ans.append(copy)
+                # make a copy of this permutation
+                ans.append(perm[:i] + [firstnum] + perm[i:])
         return ans
