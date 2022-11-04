548557ed8f51bbf355fe33f53546ec5b4fed6074
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-03 21:59:19 -0500

    Time: 72 ms (19.66%), Space: 13.8 MB (94.39%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,16 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        binary = bin(n)[2:]
-        reverse = binary[::-1]
-        fixed = reverse + "0"*(32-len(reverse))
-        return int(fixed, 2)
+        # start with the answer being 
+        answer = 0
+        # we need to get all 32 bits
+        for _ in range(32):
+            # get the last bit of n by using n & 1
+            last_bit_of_n = n & 1
+            # shift all the bits in our answer to the left by 1 spot
+            answer = answer << 1
+            # add the last bit of n to the end of our answer using answer XOR last_bit_of_n
+            answer = answer ^ last_bit_of_n
+            # shift all the bits in n to the right by 1 spot
+            n = n >> 1
+        return answer
+            
