030d5abdcfc719523048d71e55678ea6cf818e51
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-04 23:46:40 -0400

    Time: 172 ms (18.72%), Space: 15 MB (63.75%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,8 +1,6 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for i in range(1,len(triangle)):
-            triangle[i][0] += triangle[i-1][0]
-            for j in range(1,i):
-                triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
-            triangle[i][-1] += triangle[i-1][-1]
-        return min(triangle[-1])
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(i+1):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
