2a0abac018f02037ae7aca4369567b6c2dd520dd
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-11 12:16:16 -0500

    Time: 38 ms (82.15%), Space: 13.8 MB (97.04%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,9 +1,9 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if Counter(s) != Counter(goal):
+        if len(s) != len(goal) or Counter(s) != Counter(goal):
             return False
         for i in range(len(s)):
             if s == goal:
                 return True
-            s = s[1:] + s[:1]
+            s = s[1:] + s[:1] # shift the first element to the end
         return False
