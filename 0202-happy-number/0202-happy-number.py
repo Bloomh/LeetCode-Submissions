71cdaafb56795c0e6e393ad818097050dcc5c971
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-10 22:58:29 -0500

    Time: 51 ms (77.35%), Space: 13.9 MB (61.61%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,13 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n != 1:
-            if n in seen:
+        seen = set() # keep track of the numbers we have already seen
+        while n != 1: # while n isn't 1
+            if n in seen: # if we have seen this number before then we will never reach 1!
                 return False
-            seen.add(n)
-            newN = 0
-            while n:
-                newN += (n%10)**2
-                n //= 10
-            n = newN
+            seen.add(n) # add this number to the one's we've seen
+            n = sum(int(dig)**2 for dig in str(n))
         return True
