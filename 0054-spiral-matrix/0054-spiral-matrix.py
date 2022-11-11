7cdbee1712f66ba1f1c5d0686b664aa93e5ace61
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-11 13:53:12 -0500

    Time: 68 ms (12.47%), Space: 13.8 MB (83.05%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,22 +1,8 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top, bot, lft, rght = 0, len(matrix), 0, len(matrix[0]) # keep track of the top, bottom, left, and right of the region we are considering
-        ans = [] # empty matrix
-        
-        while top < bot and lft < rght: # while we still have area to cover
-            ans.extend(matrix[top][lft:rght]) # add the top row
-            top += 1 # the top of our range gets lower (by increasing index)
-            
-            ans.extend([matrix[row][rght-1] for row in range(top,bot)]) # add the rightmost column
-            rght -= 1 # rightmost column becomes smaller index
-            
-            if top == bot or rght == lft: # if we no longer have any area to consider
-                return ans # return the answer
-            
-            ans.extend(matrix[bot-1][lft:rght][::-1]) # add the bottom row in reverse order (right to left)
-            bot -= 1 # bottom row is now a smaller index
-            
-            ans.extend([matrix[row][lft] for row in range(bot-1,top-1,-1)]) # add the leftmost column in reverse order (bottom to top)
-            lft += 1 # the left bound moves forward
-    
-        return ans # return the answer!
+        ans = []
+        matrix = matrix[::-1]
+        while matrix:
+            ans.extend(matrix.pop())
+            matrix = list(zip(*matrix[::-1]))
+        return ans
