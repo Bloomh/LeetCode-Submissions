d7cd0938ba818b9d9d97020d5a88f733858b990a
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-11 15:19:27 -0500

    Time: 44 ms (82.98%), Space: 13.9 MB (49.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,12 +2,13 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         def commonPref(str1,str2):
             common = ""
-            for c1,c2 in zip(str1,str2):
-                if c1 == c2:
-                    common += c1
+            for i in range(min(len(str1),len(str2))):
+                if str1[i] == str2[i]:
+                    common+=str1[i]
                 else:
                     break
             return common
+        
         pref = strs.pop()
         for s in strs:
             pref = commonPref(pref,s)
