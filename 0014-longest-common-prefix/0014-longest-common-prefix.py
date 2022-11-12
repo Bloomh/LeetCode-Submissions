1ad68c8ec200878a44825c4fcea6f77a4d50ff7b
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-12 14:21:50 -0500

    Time: 72 ms (37.77%), Space: 13.9 MB (88.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,15 +3,15 @@ class Solution:
         # helper method to get the common prefix of two strings
         def commonPrefix(str1, str2): 
             pref = "" # start with empty prefix
-            for i in range(min(len(str1), len(str2))): # stop at the end of the shorter string
-                if str1[i] == str2[i]: # if characters are the same here
-                    pref += str1[i] # add to the prefix
-                else: # otherwise, if they are different then we need to stop
-                    return pref # return the prefix
+            for c1, c2 in zip(str1,str2): # get the characters in str1 and str2 in pairs (c1, c2)
+                if c1 == c2: # if the characters are equal
+                    pref += c1 # add to prefix
+                else: # otherwise we need to stop
+                    return pref # return prefix
             return pref # return the prefix
         
-        prefix = strs[0] # start with the first prefix
-        for i in range(1, len(strs)): # for all the other indices
-            prefix = commonPrefix(prefix, strs[i]) # get the common prefix with that string
+        prefix = strs.pop() # start with the first prefix
+        for s in strs: # for all the other indices
+            prefix = commonPrefix(prefix, s) # get the common prefix with that string
         # by the end we will have gotten the longest common prefix among all strings!
         return prefix
