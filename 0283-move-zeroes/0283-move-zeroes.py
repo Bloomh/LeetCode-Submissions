8bf86c8820797b51b7744e3e127c8190927c8d51
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-12 20:15:08 -0500

    Time: 231 ms (81.06%), Space: 15.5 MB (65.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,8 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        numZeroes = 0
-        for i, num in enumerate(nums):
-            if num == 0:
-                numZeroes += 1
-            else:
-                nums[i - numZeroes], nums[i] = num, nums[i - numZeroes]
+        firstZero = 0 # index of the first zero in our growing collection of zeroes (will be our current index if we haven't yet run into any zeroes)
+        for i in range(len(nums)): # for all indices in nums
+            if nums[i] != 0: # if it is not a zero and we have seen a zero before (if we haven't then firstZero will be equal to i)
+                nums[firstZero], nums[i] = nums[i], nums[firstZero] # swap this element and the beginning of our growing bunch of zeroes
+                firstZero += 1 # index of the first zero should increase
