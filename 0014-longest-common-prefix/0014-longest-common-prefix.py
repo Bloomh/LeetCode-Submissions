ca0e0b60767258eb120d92c33f4562df2b73903e
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-12 20:26:51 -0500

    Time: 38 ms (90.83%), Space: 14 MB (49.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str: 
-        prefix = "" # start with empty prefix
-        for chars in zip(*strs): # chars will be a tuple containing all the characters in strs index by index (we use * to unpack strs before feeding it into zip)
-            if len(set(chars)) == 1: # if there is only 1 distinct character
-                prefix += chars[0] # add the character to prefix
-            else: # otherwise we reach an issue – return the prefix
+        prefix = ""
+        for c1,c2 in zip(min(strs), max(strs)):
+            if c1 == c2:
+                prefix += c1
+            else:
                 return prefix
         return prefix
