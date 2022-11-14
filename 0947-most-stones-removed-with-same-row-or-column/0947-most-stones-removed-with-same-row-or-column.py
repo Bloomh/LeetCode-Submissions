7346248deb3288de7bc73d624d6650f04aee8132
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-14 01:23:45 -0500

    Time: 3717 ms (19.81%), Space: 14.6 MB (72.25%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,22 +1,19 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        # total number of stones minus the number of islands
-
+        # helper method to remove the island of stones connected to a stone at (i,j)
         def removeIsland(i,j):
-            ind = 0
-            toRemove = []
-            ind = 0
+            inIsland = [] # stones in the same island
+            ind = 0 # index to traverse stones
             while ind < len(stones):
-                x,y = stones[ind]
-                if x == i or y == j:
-                    toRemove.append(stones.pop(ind))
-                else:
-                    ind += 1
-            for stone in toRemove:
-                removeIsland(*stone)
-                    
-        ans = len(stones)
-        while stones:
-            removeIsland(*stones.pop())
-            ans -= 1
+                x,y = stones[ind] # this stone
+                if x == i or y == j: # if it is in the same row or col
+                    inIsland.append(stones.pop(ind)) # it is in the island –– pop from stones and add to inIsland
+                else: # otherwise
+                    ind += 1 # move to the next index in stones
+            for stone in inIsland: # for all the stones connected to this one
+                removeIsland(*stone) # recursively remove the rest of the island which connects to the stone
+        ans = len(stones) # we start off assuming that we can remove every stone
+        while stones: # while there are stones left to remove
+            removeIsland(*stones.pop()) # remove the island connected to the last stone
+            ans -= 1 # one stone from this island must remain
         return ans
