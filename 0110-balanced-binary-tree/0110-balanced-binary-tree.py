ca5b986dbc72cf48b405f75b3224de6468380aa4
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-14 18:31:00 -0500

    Time: 40 ms (99.78%), Space: 18.6 MB (90.62%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,11 +6,14 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # helper method to determine the height of a node
+        # however it will return False if the tree beginning at node is not height-balanced
         def dfs(node):
-            if not node:
-                return 1
-            left, right = dfs(node.left), dfs(node.right)
-            if left == False or right == False or abs(left-right)>1:
+            if not node: # if there is no node then return 1
+                return 1 # this will be our lowest height
+            left, right = dfs(node.left), dfs(node.right) # get the height of the left and right subtrees
+            # if either subtree is NOT height balanced, then the whole tree is not height-balanced
+            if left == False or right == False or abs(left-right)>1: # if the heights of the subtrees are not within 1 of each other, then the tree isn't height balanced
                 return False
-            return max(left,right) + 1
-        return dfs(root) != False
+            return max(left,right) + 1 # the height of this tree is 1 more than the maximum height of the subtrees
+        return dfs(root) != False # if we never returned False then we never found a height-balancing issue!
