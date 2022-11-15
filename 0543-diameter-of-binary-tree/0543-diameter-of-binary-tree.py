69c529b5f870b67899315e4694b59de8996f2931
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-15 17:36:47 -0500

    Time: 92 ms (49.44%), Space: 16.4 MB (43.37%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,12 +6,10 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.diameter = 0 # keep track of the diameter
         def dfs(node): # helper method to find the height of a node
-            if not node: # empty nodes will be considered to have height of 0
-                return 0
-            left,right = dfs(node.left),dfs(node.right) # get the height of the left and the right subnodes
-            self.diameter = max(self.diameter, right+left) # the maximum diameter could be the left height + the right height – update if necessary
-            return max(left,right)+1 # return the height of this node (one greater than the height of the subnodes)
-        dfs(root) # go through the nodes 
-        return self.diameter # return the maximum diameter
+            if not node: # empty nodes will be considered to have diameter and height of 0 and
+                return (0,0)
+            (left_diameter,left_height) = dfs(node.left) # get the diameter and height of the left subnode
+            (right_diameter,right_height) = dfs(node.right) # get the diameter and height of the right subnode
+            return (max(left_diameter,right_diameter,left_height+right_height), max(left_height,right_height)+1) # return the maximum diameter and the height of this node
+        return dfs(root)[0] # return the maximum diameter
