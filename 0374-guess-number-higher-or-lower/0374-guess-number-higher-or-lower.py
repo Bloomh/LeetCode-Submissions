d04a521764aa5dd570146ec925e6c31ee9ae14d6
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-15 19:08:55 -0500

    Time: 35 ms (87.61%), Space: 13.8 MB (66.34%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -7,15 +7,14 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        l,r = 0,n # left and right pointers keep 
-        while l <= r:
-            mid = l+(r-l)//2
-            guess_result = guess(mid)
-            if guess_result == 1:
-                l = mid + 1
-            elif guess_result == -1:
-                r = mid - 1
-            else:
-                return mid
-        return mid
+        l,r = 0,n # left and right pointers keep track of the range of guesses we are still considering ([l,r] inclusive)
+        while l <= r: # while [l,r] still contains valid area to search 
+            mid = l+(r-l)//2 # find the middle of the range
+            guess_result = guess(mid) # guess the middle
+            if guess_result == 1: # if our guess is too small
+                l = mid + 1 # then the number must be bigger than mid, so update our range to be [mid+1,r]
+            elif guess_result == -1: # if our guess is too big
+                r = mid - 1 # then the number must be smaller than mid, so update our range to be [l,mid-1]
+            else: # otherwise we guessed right!
+                return mid # return the index
         
