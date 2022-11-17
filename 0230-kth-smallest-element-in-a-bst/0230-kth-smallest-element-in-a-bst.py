767ab14be3ee7f460c83a9d95309d041ed51d7d9
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-17 14:42:20 -0500

    Time: 107 ms (49.63%), Space: 18 MB (89.49%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,8 +7,11 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         def inorder(node):
-            if node:
-                return inorder(node.left)+[node.val]+inorder(node.right)
-            return []
-        inorder_traversal = inorder(root)
-        return inorder_traversal[k-1]
+            if node: # if the node exists
+                yield from inorder(node.left) # yield values from the inorder traversal of nodes to the left of it
+                yield node.val # yield this value
+                yield from inorder(node.right) # yield values from the inorder traversal of nodes to the right of it
+        inorder_traversal = inorder(root) # 
+        for _ in range(k-1):
+            next(inorder_traversal)
+        return next(inorder_traversal)
