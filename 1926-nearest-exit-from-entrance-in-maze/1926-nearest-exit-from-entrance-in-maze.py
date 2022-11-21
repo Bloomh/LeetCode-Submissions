fa4c52c5ae95d19d6b3c8a915f4bb48410732bd8
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-21 17:55:46 -0500

    Time: 757 ms (97.16%), Space: 14.6 MB (55.92%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,23 +1,19 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        m = len(maze)
-        n = len(maze[0])
-        e1,e2 = entrance
-        maze[e1][e2] = '+';
-        queue = deque()
+        m = len(maze) # store the length of the maze
+        n = len(maze[0]) # store the width of the maze
         
-        for (r,c) in [(e1,e2-1),(e1-1,e2),(e1,e2+1),(e1+1,e2)]:
-            if 0 <= r < m and 0 <= c < n and maze[r][c] == '.':
-                queue.appendleft((r,c,1))
+        maze[entrance[0]][entrance[1]] = '+' # set the starting spot to a wall
+        queue = deque([(entrance[0],entrance[1],0)])
                 
         while queue:
             i,j,steps = queue.pop()
-            if i == 0 or j == 0 or i == m-1 or j == n-1:
+            if steps != 0 and (i == 0 or j == 0 or i == m-1 or j == n-1):
                 return steps
             else:
                 steps += 1
                 for (r,c) in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
-                    if maze[r][c] == '.':
+                    if 0 <= r < m and 0 <= c < n and maze[r][c] == '.':
                         queue.appendleft((r,c,steps))
                         maze[r][c] = '+'
         return -1
