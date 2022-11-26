8918fb9953209204f02e378d585c7a4553d0578f
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-25 20:01:41 -0500

    Time: 57 ms (49.49%), Space: 13.8 MB (75.91%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,8 +6,8 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        if not p and not q: # if neither tree exists then they are the same
             return True
-        if not p or not q:
+        if not p or not q: # if only one tree exists then there is a structural difference
             return False
-        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right) # return true if the value of both nodes is the same and they have the same left subtrees and right subtrees
