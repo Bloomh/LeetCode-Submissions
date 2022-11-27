b5cec322932b5e0b7b4e6e6e10985a088ef7156b
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-26 23:19:38 -0500

    Time: 130 ms (97.42%), Space: 14 MB (80.66%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,6 +1,12 @@
+@cache
+def power(x):
+    if x == 1: # base case
+        return 1
+    elif x % 2: # if x is odd
+        return 1 + power(3*x+1) # we need the power of 3*x+1
+    else: # if x is even
+        return 1 + power(x//2) # we need the power of x//2
+
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        @lru_cache(None)
-        def power(x):
-            return 1 if x ==1 else 1 + power(3*x+1) if x % 2 else 1 + power(x//2)
-        return heapq.nsmallest(k, range(lo,hi+1), key = power)[-1]
+        return heapq.nsmallest(k,range(lo,hi+1),key=power)[-1]
