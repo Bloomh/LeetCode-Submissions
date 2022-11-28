230009be561d1d77b19f7d877349e38f19d7d128
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-28 18:14:08 -0500

    Time: 193 ms (64.33%), Space: 15.1 MB (73.03%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,15 +1,14 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-        ans = []
-        for a in asteroids:
-            if a > 0:
-                stack.append(a)
-            else:
-                while stack and stack[-1] <= -a:
-                    if stack.pop() == -a:
-                        a = 0
-                        break
-                if a != 0 and not stack:
-                    ans.append(a)
-        return ans + stack
+        rights = [] # all right-facing asteroids
+        lefts = [] # all left-facing asteroids
+        for a in asteroids: # for every asteroid
+            if a > 0: # if it is positive then it is going to the right 
+                rights.append(a)
+            else: # if it is negative then it is going to the left
+                while rights and rights[-1] <= -a: # while there are asteroids going to the right which are not bigger than this asteroid (if they are bigger then they will destroy it) 
+                    if rights.pop() == -a: # if the last asteroid is equal to this one
+                        a = 0 # then this asteroid gets destroyed
+                if a != 0 and not rights: # if the asteroid was not destroyed and there are no right-facing asteroids remaining then this asteroid survived!
+                    lefts.append(a) # it's going to the left so add it to lefts
+        return lefts + rights # add all the left facing asteroids and then all the right facing asteroids
