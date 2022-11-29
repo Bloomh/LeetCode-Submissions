409ba80ffe5adfe69c2c717af5c478d5921b435a
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-28 19:40:37 -0500

    Time: 399 ms (97.67%), Space: 61.4 MB (57.51%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,22 +1,33 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.nums = set()
+        self.map = {} # map the values to their index in self.values
+        self.values = [] # this will store all the values of our elements
 
     def insert(self, val: int) -> bool:
-        if val in self.nums:
+        if val in self.map: # if the value is present then we do nothing and return False
             return False
-        self.nums.add(val)
+        self.map[val] = len(self.values) # map this value to the end of our list
+        self.values.append(val) # add the value to the end of the list
+
         return True
 
     def remove(self, val: int) -> bool:
-        if val in self.nums:
-            self.nums.remove(val)
-            return True
-        return False
+        if val not in self.map: # if we don't have the value in our map then we cannot remove it
+            return False
+        
+        pos_of_val = self.map[val] # get the position of the value
+        
+        self.values[pos_of_val], self.values[-1] = self.values[-1], self.values[pos_of_val] # swap the last value in our list and val
+        self.map[self.values[pos_of_val]] = pos_of_val # now update the map to know where the other value got swapped to
+        
+        self.values.pop() # remove the last value (which is now val) from our list
+        self.map.pop(val) # pop val from our map so we know we don't have it anymore
+
+        return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.nums))
+        return random.choice(self.values) # get a random value from the values
 
 
 # Your RandomizedSet object will be instantiated and called as such:
