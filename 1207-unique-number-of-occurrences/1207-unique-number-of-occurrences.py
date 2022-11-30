946b3d79e3412d134bbbbc04ee46c24b7fdcaeca
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-29 19:39:41 -0500

    Time: 83 ms (13.44%), Space: 14.1 MB (34.11%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,3 +1,3 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        return len(set((c := Counter(arr)).values())) == len(set(c))
+        return len(set((c := Counter(arr)).values())) == len(c)
