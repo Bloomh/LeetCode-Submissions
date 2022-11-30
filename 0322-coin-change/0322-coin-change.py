5e7fac12cd4ed2bb6692a98ae5c2a06c6101cad0
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-11-30 17:14:34 -0500

    Time: 4229 ms (27.46%), Space: 14.4 MB (41.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,9 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # we will use a list, dp, to keep track of the minimum number of coins needed
+        # dp[i] will represent the minimum number of coins needed to sum to i
+        # originally, every index from 1 to amount will need to be infinity since we have no way to sum
         dp = [0]+[inf]*(amount)
+        
+        # for every coin, we will update dp
         for c in coins:
+            # we only care if the coin is worth less than our amount, otherwise we can't use it
             if c <= amount:
+                # it takes only 1 coin to sum to this coin's value
                 dp[c] = 1
+                
+                # for all values from c+1 to amount (we don't care about smaller or bigger values)
                 for i in range(c+1,amount+1):
+                    # see if we can sum to i using fewer coins
+                    # if dp[i-c] isn't infinity, then we can use our new coin to sum to i
+                    # this takes 1 extra coin, so we check if 1 + dp[i-c] is less than dp[i]
                     dp[i] = min(dp[i], 1+dp[i-c])
-        return dp[-1] if dp[-1] != inf else -1
+                    
+        # if we never found a valid combination then we return -1
+        if dp[amount] == inf:
+            return -1
+        
+        # otherwise just return the min number of coins to sum to amount!
+        return dp[amount]
