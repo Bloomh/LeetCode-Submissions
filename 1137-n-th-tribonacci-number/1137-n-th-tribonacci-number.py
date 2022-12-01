461c7056e622d1bc35b4fc2351388abbe05e202f
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-12-01 13:20:20 -0500

    Time: 52 ms (57.21%), Space: 13.9 MB (59.63%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        if n < 2:
+            return n
         tribonnaci_nums = (0,1,1) # the first three tribonnaci numbers are 0, 1, and 1
-        while n > 2:
+        for i in range(3,n+1):
             tribonnaci_nums = (tribonnaci_nums[-2], tribonnaci_nums[-1] , tribonnaci_nums[-1] + tribonnaci_nums[-2] + tribonnaci_nums[-3]) # the next tribonnaci number is the sum of the last three!
-            n -= 1
-        return tribonnaci_nums[n] # return the nth tribonnaci number
+        return tribonnaci_nums[-1] # return the nth tribonnaci number
