25c9570a12c419650abe7b7c8b5c25d9d816408d
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-12-01 01:37:41 -0500

    Time: 75 ms (34.41%), Space: 13.9 MB (33.58%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,12 +1,24 @@
+# we will use a set to keep track of all vowels
+vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']) 
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        # we will use a set to keep track of the 
-        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']) 
+        # get half the length of the string
         half = len(s)//2
+        
+        # difference will keep track of the number of vowels in the first half minus the number of vowels in the second half
         difference = 0
+        
+        # go through half of the string
         for i in range(half):
+            # if the character in the first half is a vowel
             if s[i] in vowels:
+                # add to the difference
                 difference += 1
+                
+            # if the character in the second half is a vowel
             if s[i+half] in vowels:
+                # subtract from the difference
                 difference -= 1
+        
+        # if the difference is zero, then the number of vowels in the first and second half is the same!
         return difference == 0
