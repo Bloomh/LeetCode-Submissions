c73e7f6b47e331ac9128dada3391b162274424f6
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-12-02 19:58:09 -0500

    Time: 130 ms (24.85%), Space: 19.3 MB (13.86%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,4 +1,30 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        c = Counter(s)
-        return ''.join(sorted(s, key = lambda x:(c[x],x), reverse = True))
+        # we use a hashmap to tore the frequency of each character in s
+        # where counts[char] is the amount of times char is in s
+        counts = Counter(s) 
+        
+        # our list will have buckets to store characters by their frequency
+        # buckets[i] will store all the characters which appear in s a total of i times (so all the chars such that counts[char] == i)
+        buckets = [] 
+        
+        # we need to iterate over all the keys and values in our hashmap (all the unique characters in s and their frequency)
+        for char,freq in counts.items(): 
+            # while there aren't enough buckets to store this character
+            while len(buckets) <= freq:
+                # add a new bucket
+                buckets.append([])
+            
+            # add this character to the corresponding bucket, as many times as it needs to be in our final string (freq times)
+            buckets[freq].append(char*freq)
+        
+        # initialize an empty answer string
+        ans = ""
+        
+        # we need to iterate over the buckets backwards since we want decreasing order
+        while buckets:
+            # pop the last bucket and add its string to the answer (we need to turn the list into a string with ''.join())
+            ans += ''.join(buckets.pop())
+            
+        # return our final answer!
+        return ans
