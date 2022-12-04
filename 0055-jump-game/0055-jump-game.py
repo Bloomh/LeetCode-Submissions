59d9ce13c2fcc5c5d9cdc0a933a2385a737595e2
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-12-03 19:46:31 -0500

    Time: 9208 ms (5.01%), Space: 15.2 MB (49.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,20 +1,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # store the length of the array
         n = len(nums)
-        
-        # keep track of the furthest index we can get to (initially 0)
-        furthest = 0
+        dp = [True] + [False]*(n-1)
         
         for i in range(n):
-            # if we can get to this step
-            if i <= furthest:
-                # update the furthest step we can go to if (nums[i] + i) > furthest
-                furthest = max(furthest, nums[i] + i)
-                
-                # if we can reach the end then we should return true!
-                if furthest >= n - 1:
+            if dp[i]:
+                s = nums[i]+i
+                if s < n - 1:
+                    for j in range(i+1,s+1):
+                        dp[j] = True
+                else:
                     return True
-                
-            else: # if we can't get to this step, we can't reach the end – return False
-                return False
+        return False
