019a093bfac1d25437a314f431f9b3cf626045c6
Henry Bloom <50490198+Bloomh@users.noreply.github.com>
2022-12-03 19:34:00 -0500

    Time: 8755 ms (5.01%), Space: 15.3 MB (17.69%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [True] + [False]*(n-1)
+        
+        for i in range(n):
+            if dp[i]:
+                s = nums[i]+i
+                if s < n - 1:
+                    for j in range(i+1,s+1):
+                        dp[j] = True
+                else:
+                    return True
